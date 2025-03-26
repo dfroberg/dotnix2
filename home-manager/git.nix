0e946de3f5ba8054b183c7122f3e54ce0e9497db
Danny Froberg <danny@froberg.org>
2025-03-26 15:18:52 +0100

    fix(gitconfig): update include configuration for custom git settings

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -4,6 +4,10 @@
     ".cvsignore".source = ../.config/git/.cvsignore;
     ".gitconfig".source = ../.config/git/.gitconfig;
     ".gitconfig-tain".source = ../.config/git/.gitconfig-tain;
+    ".gitconfig-custom".text = ''
+      [includeIf "gitdir:${config.home.homeDirectory}/eeze/**/.git"]
+        path = ~/.gitconfig-tain
+    '';
   };
 
   programs.git = {
